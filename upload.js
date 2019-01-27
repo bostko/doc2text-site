4f399af854a05a04930790fd914777bdd40f7462
Valentin Aitken <valentin.aitken@azdio.com>
2019-01-27 17:45:34 +0200

    Process odt files using doc2text-api

---
+++ upload.js
@@ -0,0 +1,19 @@
+const url = 'https://doc2text-api.herokuapp.com/process_binary/odt';
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+console.log('Adding listener');
+  const file = document.querySelector('[type=file]').files;
+  const formData = new FormData();
+  const markdownContent = document.getElementById('markdownContent');
+  formData.append('odt_file', file[0]);
+
+  fetch(url, {
+    method: 'POST',
+    body: formData
+  }).then(response => {
+    response.text().then(function(val) {
+      markdownContent.innerHTML = val;});
+  });
+});
