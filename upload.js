c92745982e958ca3b55c64493ae14dc5b943f474
Valentin Aitken <valentin@nalisbg.com>
2019-10-05 11:43:03 +0300

    Add loading spinner

--- upload.js
+++ upload.js
@@ -1,9 +1,20 @@
 const url = 'https://doc2text-api.herokuapp.com/process_binary/odt';
 const form = document.querySelector('form');
 
+function startSpinner() {
+  var loader = document.getElementsByClassName('loader')[0];
+  loader.style.display = 'block';
+}
+
+function stopSpinner() {
+  var loader = document.getElementsByClassName('loader')[0];
+  loader.style.display = 'none';
+}
+
 form.addEventListener('submit', e => {
   e.preventDefault();
   const file = document.querySelector('[type=file]').files;
+  startSpinner();
   const formData = new FormData();
   const markdownContent = document.getElementById('markdownContent');
   formData.append('odt_file', file[0]);
@@ -13,6 +24,8 @@ form.addEventListener('submit', e => {
     body: formData
   }).then(response => {
     response.text().then(function(val) {
-      markdownContent.innerHTML = val;});
+      stopSpinner();
+      markdownContent.innerHTML = val;
+    });
   });
 });
